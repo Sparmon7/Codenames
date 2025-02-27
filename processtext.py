f04fc5d1b17789e9d9f7f6f8690eec545158c8a8
Spencer Armon <89878656+Sparmon7@users.noreply.github.com>
2025-02-26 22:09:14 -0600

    larger embeddings

---
+++ processtext.py
@@ -0,0 +1,30 @@
+import msgpack
+import lzma
+import numpy as np
+
+
+def encode_numpy(obj):
+    if isinstance(obj, np.ndarray):
+        return {
+            "__ndarray__": obj.tobytes(),
+            "dtype": str(obj.dtype),
+            "shape": obj.shape
+        }
+    return obj
+
+def decode_numpy(obj):
+    if "__ndarray__" in obj:
+        return np.frombuffer(obj["__ndarray__"], dtype=obj["dtype"]).reshape(obj["shape"])
+    return obj
+
+
+embeddings = {}
+with open("glove.6B.200d.txt", "r", encoding="utf-8") as f:
+    for line in f:
+        vec = line.rstrip("\n").split(" ")
+        if vec[0].isalpha():
+            embeddings[vec[0]] = np.round(np.array(vec)[1:].astype(float),2)
+        
+print('saving')
+with lzma.open("embeddings.msgpack.xz", "wb") as f:
+    f.write(msgpack.packb(embeddings, default=encode_numpy))
